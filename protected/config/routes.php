f675ad03e53bb1b625d9c3d98b75a91be21fcdc5
Lyashkov Pavel <bilbo.kem@gmail.com>
2012-08-31 15:43:06 +0200

    fix

--- protected/config/routes.php
+++ protected/config/routes.php
@@ -3,9 +3,7 @@ return array(
     'showScriptName' => false,
     'urlFormat' => 'path',
     'rules' => array(
-        'http://m.mobispot.test' => 'mobile',
-        'http://m.mobispot.test' => 'mobile',
-        'http://m.mobispot.test' => 'mobile',
+        'http://mobile.mobispot.spb4.webdoka.ru/' => 'mobile',
         '<controller:\w+>/<id:\d+>' => '<controller>/view',
         '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
         '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
