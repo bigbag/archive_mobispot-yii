6f0ef93e8da5176e8dc1498a9689b32dc625b51e
Pavel Lyashkov <bilbo.kem@gmail.com>
2012-08-31 18:59:29 +0400

    fix mobile 2

---
+++ protected/views/layouts/block/header.php
@@ -0,0 +1,11 @@
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=cp1251"/>
+    <title><?php echo Yii::app()->par->load('siteTitle'); ?></title
+    <link rel="icon" type="image/png" href="/themes/mobispot/images/favicon.ico" />
+    <?php Yii::app()->getClientScript()->registerCssFile('/themes/mobile/css/styles.css' ); ?>
+
+    <?php Yii::app()->getClientScript()->registerScriptFile('/themes/mobispot/js/niceCheckbox.js' ); ?>
+    <?php Yii::app()->getClientScript()->registerScriptFile('/themes/mobispot/js/jquery.redirect.min.js'); ?>
+    <?php Yii::app()->getClientScript()->registerScriptFile('/themes/mobispot/js/jquery.placeholder.js' ); ?>
+    <?php Yii::app()->getClientScript()->registerCoreScript('jquery');?>
+</head>
---
+++ protected/views/layouts/mobile.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html PUBLIC "-//WAPFORUM//DTD XHTML Mobile 1.0//EN" "http://www.wapforum.org/DTD/xhtml-mobile10.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<?php include('block/header.php');?>
+<body>
+<div id="wrapper">
+    <div id="header">
+        <a href="#" id="logo"><img src="/themes/mobile/images/logo.png" alt=""></a>
+    </div>
+    <div id="errors"></div>
+    <?php echo $content; ?>
+</div>
+<div class="center-rel">
+    <?php include('block/footer.php');?>
+</div>
+</body>
+</html>
+
+
+
+
