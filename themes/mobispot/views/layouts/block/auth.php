283203413dad35dd173e66fe0fc23c4916af541c
Pavel Lyashkov <bilbo.kem@gmail.com>
2012-09-04 15:28:20 +0400

    fix auth modal form

--- themes/mobispot/views/layouts/block/auth.php
+++ themes/mobispot/views/layouts/block/auth.php
@@ -27,21 +27,34 @@
                 <span class="remember-me">
                     <?php echo Yii::t('user', 'Запомнить меня')?>
                 </span>
+                <table style="width: 100%">
+                    <tr>
+                        <td>
+                            <a class="auth-link facebook" href="/service/social?service=facebook" title="facebook"><img
+                                src="/themes/mobispot/images/auth_facebook.png" alt="fb"></a>
+                            <a class="auth-link twitter" href="/service/social?service=twitter" title="twitter"><img
+                                src="/themes/mobispot/images/auth_twitter.png" alt="tw"></a>
+                            <a class="auth-link google_oauth" href="/service/social?service=google_oauth"
+                               title="google"><img
+                                src="/themes/mobispot/images/auth_google.png" alt="g"></a>
 
-                <div class="btn-30">
-                    <div><input type="submit" id="hint-button-auth" value="<?php echo Yii::t('user', 'Отправить');?>"/>
-                    </div>
-                </div>
-            </td>
-        </tr>
-        <tr>
-            <td>
-                <span id="mistake-auth" style="display:none;text-align: right"></span>
-            </td>
-        </tr>
-        <tr>
-            <td style="text-align: right">
-                <span class="forget-pass"><?php echo Yii::t('user', 'Забыли пароль?')?></span>
+                        </td>
+                        <td>
+                            <div class="btn-30" style="float: right">
+                                <div><input type="submit" id="hint-button-auth"
+                                            value="<?php echo Yii::t('user', 'Отправить');?>"/>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan=2 style="text-align: right">
+                            <span class="forget-pass"><?php echo Yii::t('user', 'Забыли пароль?')?></span>
+                            <span id="mistake-auth" style="display:none;"></span>
+                        </td>
+                    </tr>
+                </table>
+
             </td>
         </tr>
     </table>
