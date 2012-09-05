597929b6fba71e79ed011c49f10ab9f883f6d41c
Pavel Lyashkov <bilbo.kem@gmail.com>
2012-09-05 17:39:31 +0400

    fix

--- protected/modules/mobile/views/widget/spot/file.php
+++ protected/modules/mobile/views/widget/spot/file.php
@@ -1,6 +1,6 @@
 <?php if (strpos($content->fayl_6, 'pdf')  == 0): ?>
 <img src="/uploads/spot/<?php echo $content->fayl_6;?>" alt=""/>
 <?php else: ?>
-<iframe src="http://docs.google.com/gview?url=http://<?php echo Yii::app()->request->getBaseUrl(true);?>/<?php echo $content->fayl_6;?>" style="width:400px; height:700px;" frameborder="0"></iframe>
+<iframe src="http://docs.google.com/gview?url=<?php echo Yii::app()->request->getBaseUrl(true);?>/uploads/spot/<?php echo $content->fayl_6;?>" style="width:400px; height:700px;" frameborder="0"></iframe>
 
 <?php endif; ?>
