c9bf26e8da35dd2974b296a7d09693a8b4addd76
Pavel Lyashkov <bilbo.kem@gmail.com>
2012-09-05 14:34:33 +0400

    add send spot type

--- themes/mobispot/views/widget/spot/send.php
+++ themes/mobispot/views/widget/spot/send.php
@@ -1,30 +1,48 @@
-<p><?php echo Yii::t('account', 'Отправьте важные файлы тому, кто просканирует Ваш спот. Ему будет <br />
+<form action="" method="post" class="spot_edit_content" id="spot_edit_content_<?php echo $content->spot_id?>">
+    <?php echo CHtml::activeHiddenField($content, 'spot_id'); ?>
+    <?php echo CHtml::activeHiddenField($content, 'spot_type_id'); ?>
+    <p><?php echo Yii::t('account', 'Отправьте важные файлы тому, кто просканирует Ваш спот. Ему будет <br />
     достаточно ввести свой адрес элетронной почты, и он их незамедлительно получит.<br />
-    Загрузите до 5 разных файлов (общий размер - до 10 Мб), и они будут отправлены тому, кто об этом попросит.<br />');?>
-</p>
-<table class="mailInfoTbl" cellspacing="0">
-    <tr>
-        <td>
-            <a href="" class="btn-30">
-                <span class="upload-ico ico"></span>
-                <span class="btn-30-txt"><?php echo Yii::t('account', 'Загрузить файл');?></span>
-            </a>
-        </td>
-    </tr>
-    <tr>
-        <td>
-            <a href="" class="btn-30">
-                <span class="upload-ico ico"></span>
-                <span class="btn-30-txt"><?php echo Yii::t('account', 'Загрузить файл');?></span>
-            </a>
-        </td>
-    </tr>
-    <tr>
-        <td>
-            <a href="" class="btn-30">
-                <span class="upload-ico ico"></span>
-                <span class="btn-30-txt"><?php echo Yii::t('account', 'Загрузить файл');?></span>
-            </a>
-        </td>
-    </tr>
-</table>
+    Загрузите до 3 разных файлов (общий размер - до 10 Мб), и они будут отправлены тому, кто об этом попросит.<br />');?>
+    </p>
+    <?php echo CHtml::activeHiddenField($content, 'fayl-1_10', array('id' => 'spot_send_field1_' . $data->discodes_id)); ?>
+    <?php echo CHtml::activeHiddenField($content, 'fayl-2_10', array('id' => 'spot_send_field2_' . $data->discodes_id)); ?>
+    <?php echo CHtml::activeHiddenField($content, 'fayl-3_10', array('id' => 'spot_send_field3_' . $data->discodes_id)); ?>
+
+    <div class="round-btn-upload">
+        <input type="submit" id="add_file_<?php echo($data->discodes_id)?>" class=""
+               value="<?php echo Yii::t('account', 'Загрузить');?>"/>
+    </div>
+</form>
+
+<script type="text/javascript">
+    $(function () {
+        var i = 1;
+        $("#add_file_<?php echo($data->discodes_id)?>").uploadify({
+            'width':'110',
+            'height':'30',
+            swf:'/themes/mobispot/js/uploadify.swf',
+            uploader:'/user/uploadFile/',
+            'formData':{'spot_id':<?php echo($data->discodes_id)?>},
+            'removeTimeout':10,
+            'multi':true,
+            'uploadLimit':3,
+            'buttonClass':'uploadify_file',
+            'buttonText':'<?php echo Yii::t('profile', 'Загрузить');?>',
+
+            'onUploadError':function (file, errorCode, errorMsg, errorString) {
+                alert('The file ' + file.name + ' could not be uploaded: ' + errorString);
+            },
+            'onUploadSuccess':function (file, data, response) {
+                var obj = jQuery.parseJSON(data);
+                var file_name = obj.file;
+                var error = obj.error;
+                if (error) alert(error);
+                if (file_name) {
+                    $('#spot_send_field' + i + '_<?php echo $data->discodes_id; ?>').val(file_name);
+                    i = i + 1;
+                }
+            }
+        });
+    });
+</script>
