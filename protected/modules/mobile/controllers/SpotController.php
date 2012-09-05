1f9ef2cc732dd746ff615534ccf00c812bef6059
Pavel Lyashkov <bilbo.kem@gmail.com>
2012-09-05 18:03:47 +0400

    fix file mobile view

--- protected/modules/mobile/controllers/SpotController.php
+++ protected/modules/mobile/controllers/SpotController.php
@@ -11,20 +11,29 @@ class SpotController extends MController
             $spot = Spot::model()->findByAttributes(array('url' => $url));
             if ($spot) {
                 $content = SpotModel::model()->findByAttributes(array('spot_id' => $spot->discodes_id, 'spot_type_id' => $spot->spot_type_id));
-                $txt = $this->renderPartial('/widget/spot/' . $spot->spot_type->pattern,
-                    array(
-                        'data' => $spot,
+
+
+                if ($spot->spot_type->pattern == 'file') {
+                    $url = Yii::app()->request->getBaseUrl(true) . '/uploads/spot/' . $content->fayl_6;
+
+                    header("Location: " . $url);
+                } else {
+
+                    $txt = $this->renderPartial('/widget/spot/' . $spot->spot_type->pattern,
+                        array(
+                            'data' => $spot,
+                            'content' => $content,
+                        ),
+                        true);
+                    $this->render('view', array(
+                        'txt' => $txt,
+                        'spot' => $spot,
                         'content' => $content,
-                    ),
-                    true);
-                $this->render('view', array(
-                    'txt' => $txt,
-                    'spot' => $spot,
-                    'content' => $content,
-                ));
-            }
-            else throw new CHttpException(404, 'The requested page does not exist.');
-        }
-        else throw new CHttpException(404, 'The requested page does not exist.');
+                    ));
+
+                }
+            } else
+                throw new CHttpException(404, 'The requested page does not exist.');
+        } else throw new CHttpException(404, 'The requested page does not exist.');
     }
 }
