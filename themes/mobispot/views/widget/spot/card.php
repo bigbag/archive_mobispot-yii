4b2383b92aa0ef318c552cbffedcc712041948f4
Pavel Lyashkov <bilbo.kem@gmail.com>
2012-09-07 16:38:05 +0400

    fix card spot type 2

--- themes/mobispot/views/widget/spot/card.php
+++ themes/mobispot/views/widget/spot/card.php
@@ -12,153 +12,155 @@
 
 <div class="oneSpotInfo">
     <form action="" method="post" class="spot_edit_content" id="spot_edit_content_<?php echo $content->spot_id?>">
-    <table class="visitInfoTbl" cellspacing="0">
-        <tbody>
-        <tr>
-            <td class="field"><?php echo Yii::t('account', 'Название бизнеса');?></td>
-            <td>
-                <div class="txt-form">
-                    <div class="txt-form-cl">
-                        <?php echo CHtml::activeTextField($content, 'nazvanie-biznesa_8', array('class' => 'text')); ?>
-                    </div>
-                </div>
-            </td>
-        </tr>
-        <tr>
-            <td class="field"><?php echo Yii::t('account', 'Веб сайт');?></td>
-            <td>
-                <div class="txt-form">
-                    <div class="txt-form-cl">
-                        <?php echo CHtml::activeTextField($content, 'sayt_8', array('class' => 'text')); ?>
-                    </div>
-                </div>
-            </td>
-        </tr>
-        <tr>
-            <td class="field"><?php echo Yii::t('account', 'Контактное лицо');?></td>
-            <td>
-                <div class="txt-form">
-                    <div class="txt-form-cl">
-                        <?php echo CHtml::activeTextField($content, 'kontaktnoe-litso_8', array('class' => 'text')); ?>
-                        <input class="txt" name="inputtext" value="" placeholder=" " type="text">
-                    </div>
-                </div>
-            </td>
-        </tr>
-        </tbody>
-    </table>
-    <div class="action">
-        <br/>
-        <br/>
+        <?php echo CHtml::activeHiddenField($content, 'spot_id'); ?>
+        <?php echo CHtml::activeHiddenField($content, 'spot_type_id'); ?>
         <table class="visitInfoTbl" cellspacing="0">
             <tbody>
             <tr>
-                <td class="field vatop" rowspan="4"><?php echo Yii::t('account', 'Интересное предложение');?></td>
+                <td class="field"><?php echo Yii::t('account', 'Название бизнеса');?></td>
                 <td>
                     <div class="txt-form">
                         <div class="txt-form-cl">
-                            <?php echo CHtml::activeTextField(
-                            $content,
-                            'aktsiya-nazvanie_8[]',
-                            array(
-                                'class' => 'text',
-                                'placeholder' => Yii::t('account', 'Название'),
-                            ));
-                            ?>
+                            <?php echo CHtml::activeTextField($content, 'nazvanie-biznesa_8', array('class' => 'text')); ?>
                         </div>
                     </div>
                 </td>
             </tr>
             <tr>
+                <td class="field"><?php echo Yii::t('account', 'Веб сайт');?></td>
                 <td>
-                    <?php echo CHtml::activeHiddenField($content, 'aktsiya-kartinka_8', array('id' => 'spot_card_field_' . $data->discodes_id)); ?>
-                    <div class="round-btn-upload">
-                        <input type="submit" id="add_file_<?php echo($data->discodes_id)?>" class=""
-                               value="<?php echo Yii::t('account', 'Загрузить');?>"/>
+                    <div class="txt-form">
+                        <div class="txt-form-cl">
+                            <?php echo CHtml::activeTextField($content, 'sayt_8', array('class' => 'text')); ?>
+                        </div>
                     </div>
                 </td>
             </tr>
             <tr>
+                <td class="field"><?php echo Yii::t('account', 'Контактное лицо');?></td>
                 <td>
                     <div class="txt-form">
                         <div class="txt-form-cl">
-                            <?php echo CHtml::activeTextField(
-                            $content,
-                            'aktsiya-ssyilka_8[]',
-                            array(
-                                'class' => 'text',
-                                'placeholder' => Yii::t('account', 'Ссылка на веб-страницу'),
-                            ));
-                            ?>
+                            <?php echo CHtml::activeTextField($content, 'kontaktnoe-litso_8', array('class' => 'text')); ?>
+                            <input class="txt" name="inputtext" value="" placeholder=" " type="text">
                         </div>
                     </div>
                 </td>
             </tr>
             </tbody>
         </table>
-    </div>
-    <table class="visitInfoTbl" cellspacing="0">
-        <tbody>
-        <tr>
-            <td><a href="" class="r-btn-30 copy"
-                   rel=".action"><span><?php echo Yii::t('account', 'Добавить предложение');?></span></a>
-            </td>
-        </tr>
-        </tbody>
-    </table>
+        <div class="action">
+            <br/>
+            <br/>
+            <table class="visitInfoTbl" cellspacing="0">
+                <tbody>
+                <tr>
+                    <td class="field vatop" rowspan="4"><?php echo Yii::t('account', 'Интересное предложение');?></td>
+                    <td>
+                        <div class="txt-form">
+                            <div class="txt-form-cl">
+                                <?php echo CHtml::activeTextField(
+                                $content,
+                                'aktsiya-nazvanie_8[]',
+                                array(
+                                    'class' => 'text',
+                                    'placeholder' => Yii::t('account', 'Название'),
+                                ));
+                                ?>
+                            </div>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <?php echo CHtml::activeHiddenField($content, 'aktsiya-kartinka_8', array('id' => 'spot_card_field_' . $data->discodes_id)); ?>
+                        <div class="round-btn-upload">
+                            <input type="submit" id="add_file_<?php echo($data->discodes_id)?>" class=""
+                                   value="<?php echo Yii::t('account', 'Загрузить');?>"/>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <div class="txt-form">
+                            <div class="txt-form-cl">
+                                <?php echo CHtml::activeTextField(
+                                $content,
+                                'aktsiya-ssyilka_8[]',
+                                array(
+                                    'class' => 'text',
+                                    'placeholder' => Yii::t('account', 'Ссылка на веб-страницу'),
+                                ));
+                                ?>
+                            </div>
+                        </div>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+        <table class="visitInfoTbl" cellspacing="0">
+            <tbody>
+            <tr>
+                <td><a href="" class="r-btn-30 copy"
+                       rel=".action"><span><?php echo Yii::t('account', 'Добавить предложение');?></span></a>
+                </td>
+            </tr>
+            </tbody>
+        </table>
 
-    <div class="place">
-        <br/>
-        <br/>
+        <div class="place">
+            <br/>
+            <br/>
+            <table class="visitInfoTbl" cellspacing="0">
+                <tbody>
+                <tr>
+                    <td class="field vatop" rowspan="3"><?php echo Yii::t('account', 'Ближайшие точки продаж');?></td>
+                    <td>
+                        <div class="txt-form">
+                            <div class="txt-form-cl">
+                                <?php echo CHtml::activeTextField(
+                                $content,
+                                'tochka-nazvanie_8[]',
+                                array(
+                                    'class' => 'text',
+                                    'placeholder' => Yii::t('account', 'Название и адрес'),
+                                ));
+                                ?>
+                            </div>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <div class="txt-form">
+                            <div class="txt-form-cl">
+                                <?php echo CHtml::activeTextField(
+                                $content,
+                                'tochka-karta_8[]',
+                                array(
+                                    'class' => 'text',
+                                    'placeholder' => Yii::t('account', 'Ссылка на карту'),
+                                ));
+                                ?>
+                            </div>
+                        </div>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
         <table class="visitInfoTbl" cellspacing="0">
             <tbody>
             <tr>
-                <td class="field vatop" rowspan="3"><?php echo Yii::t('account', 'Ближайшие точки продаж');?></td>
                 <td>
-                    <div class="txt-form">
-                        <div class="txt-form-cl">
-                            <?php echo CHtml::activeTextField(
-                            $content,
-                            'tochka-nazvanie_8[]',
-                            array(
-                                'class' => 'text',
-                                'placeholder' => Yii::t('account', 'Название и адрес'),
-                            ));
-                            ?>
-                        </div>
-                    </div>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <div class="txt-form">
-                        <div class="txt-form-cl">
-                            <?php echo CHtml::activeTextField(
-                            $content,
-                            'tochka-karta_8[]',
-                            array(
-                                'class' => 'text',
-                                'placeholder' => Yii::t('account', 'Ссылка на карту'),
-                            ));
-                            ?>
-                        </div>
-                    </div>
+                    <a href="" class="r-btn-30 copy" rel=".place">
+                        <span><?php echo Yii::t('account', 'Добавить точку продаж');?></span>
+                    </a>
                 </td>
             </tr>
             </tbody>
         </table>
-    </div>
-    <table class="visitInfoTbl" cellspacing="0">
-        <tbody>
-        <tr>
-            <td>
-                <a href="" class="r-btn-30 copy" rel=".place">
-                    <span><?php echo Yii::t('account', 'Добавить точку продаж');?></span>
-                </a>
-            </td>
-        </tr>
-        </tbody>
-    </table>
     </form>
 </div>
 <div class="clear"></div>
