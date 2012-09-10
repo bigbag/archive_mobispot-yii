f7595c1004e59148f004bab91833c6d00c6f71bd
Pavel Lyashkov <bilbo.kem@gmail.com>
2012-09-10 13:13:47 +0400

    fix send spot type 3

---
+++ protected/modules/mobile/models/SendForm.php
@@ -0,0 +1,33 @@
+<?php
+
+
+class SendForm extends CFormModel
+{
+    public $email;
+    public $terms;
+
+    /**
+     * Declares the validation rules.
+     * The rules state that username and password are required,
+     * and password needs to be authenticated.
+     */
+    public function rules()
+    {
+        return array(
+            array('email', 'required'),
+            array('email', 'email'),
+            array('terms', 'required', 'message' => Yii::t('user', "Вы должны согласиться с условиями предоставления сервиса")),
+            array('terms', 'in', 'range' => array(1), 'message' => "Вы должны согласиться с условиями предоставления сервиса"),
+        );
+    }
+
+    /**
+     * Declares attribute labels.
+     */
+    public function attributeLabels()
+    {
+        return array(
+            'email' => Yii::t('user', "Email"),
+        );
+    }
+}
