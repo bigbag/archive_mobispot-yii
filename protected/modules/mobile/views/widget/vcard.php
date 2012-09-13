c121583c602aa31a096dde50e797d2d607c730dc
Pavel Lyashkov <bilbo.kem@gmail.com>
2012-09-13 15:55:35 +0400

    fix

--- protected/modules/mobile/views/widget/vcard.php
+++ protected/modules/mobile/views/widget/vcard.php
@@ -2,9 +2,4 @@ BEGIN:VCARD
 VERSION:3.0
 FN:<?php echo $content->imya_3; ?>
 N:<?php echo $content->imya_3; ?>
-<?php foreach ($all_field as $row): ?>
-<?php if (in_array($row['id'], $select_field)): ?>
-<?php echo $row['vcard']; ?>:<?php echo $data[$row['id']]; ?><? echo "\r\n" ?>
-<?php endif; ?>
-<?php endforeach; ?>
 END:VCARD
