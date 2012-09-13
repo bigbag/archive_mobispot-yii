c6c67a4e634b268bfbca8eaef909a91014b1ab4a
Pavel Lyashkov <bilbo.kem@gmail.com>
2012-09-13 15:58:51 +0400

    fix

--- protected/modules/mobile/views/widget/vcard.php
+++ protected/modules/mobile/views/widget/vcard.php
@@ -1,5 +1,5 @@
-BEGIN:VCARD
-VERSION:3.0
+BEGIN:VCARD<? echo "\r\n" ?>
+VERSION:3.0<? echo "\r\n" ?>
 FN:<?php echo $content->imya_3; ?><? echo "\r\n" ?>
 N:<?php echo $content->imya_3; ?><? echo "\r\n" ?>
 END:VCARD
