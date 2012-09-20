256b1379abcf2b6f1bade5d8d38cb1091523a7d9
Pavel Lyashkov <bilbo.kem@gmail.com>
2012-09-20 21:43:28 +0400

    fix

--- protected/config/eauth.php
+++ protected/config/eauth.php
@@ -3,21 +3,21 @@ return array( // You can change the providers and their classes.
     'twitter' => array(
         // register your app here: https://dev.twitter.com/apps/new
         'class' => 'TwitterOAuthService',
-        'key' => 'eX4XJnsIVOpXwNlo8xIxQ',
-        'secret' => 'Z3jH7SAFzc8bRNXaif8rLSKZcA17lSYC9AjW9qJwO0',
+        'key' => 'QlnOol9mGgTn6zZXrWZOow',
+        'secret' => 'J1eJFs0J4YlbaFyAvYEa4IXK0yQRV6HKxzchjN60M',
     ),
 
     'google_oauth' => array(
         // register your app here: https://code.google.com/apis/console/
         'class' => 'CustomGoogleOAuthService',
-        'client_id' => '419796517496.apps.googleusercontent.com',
-        'client_secret' => '25pDyrLThDlGaKxCf3UtnXU_',
+        'client_id' => '510457098983.apps.googleusercontent.com',
+        'client_secret' => 'qGZivEcZ3icszUdhKOyW9uP3',
         'title' => 'Google (OAuth)',
     ),
     'facebook' => array(
         // register your app here: https://developers.facebook.com/apps/
         'class' => 'FacebookOAuthService',
-        'client_id' => '219110061545862',
-        'client_secret' => 'dc8e6d5bb095853fe53dd2bfd63a92da',
+        'client_id' => '445335612171646',
+        'client_secret' => '03e2146d3f36d3feca0bed3dd7a9dea7',
     ),
 );
