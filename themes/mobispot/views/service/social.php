1e4a315ecc93fbf5d0ec20906a37197e5e347eaa
Lyashkov Pavel <bilbo.kem@gmail.com>
2012-10-01 14:40:31 +0400

    fix socail registration 3

--- themes/mobispot/views/service/social.php
+++ themes/mobispot/views/service/social.php
@@ -1,13 +1,6 @@
 <?php $this->beginContent('//layouts/general'); ?>
 <?php if (Yii::app()->user->isGuest): ?>
-    <?php switch($service):?>
-        <?php case('twitter'):?>
-            <?php include('block/_not_auth_with_email.php'); ?>
-        <?php break;?>
-        <?php default:?>
-            <?php include('block/_not_auth_no_email.php'); ?>
-        <?php break;?>
-    <?php endswitch;?>
+<?php include('block/_not_auth.php'); ?>
 <?php else: ?>
 <?php include('../site/block/_auth.php'); ?>
 <?php endif; ?>
