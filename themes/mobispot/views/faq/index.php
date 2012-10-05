45bdb34053b1b2a210f877b7c0c4a20578bb147c
Lyashkov Pavel <bilbo.kem@gmail.com>
2012-10-05 19:15:09 +0400

    fix faq pages

--- themes/mobispot/views/faq/index.php
+++ themes/mobispot/views/faq/index.php
@@ -1,128 +1,132 @@
-<div id="main-container">
-    <div id="cont-block" class="center">
-        <div id="cont-block-900" class="center">
-            <div id="zag-cont-block"><?php echo Yii::t('faq', 'Часто задаваемые вопросы');?></div>
-            <div id="blockFAQ">
-                <ul>
-                    <?php foreach ($model as $row): ?>
-                    <li class="mmenu">
-                        <a href="#" class="akordeon"><span><?php echo $row->question;?></span></a>
+<div class="page">
+    <div class="title"><?php echo Yii::t('faq', 'Часто задаваемые вопросы');?></div>
+    <div class="content faq">
+        <ul>
+            <?php foreach ($model as $row): ?>
+            <li class="mmenu">
+                <a href="#" class="akordeon"><span><?php echo $row->question;?></span></a>
 
-                        <div class="answer">
-                            <?php echo $row->answer;?>
-                        </div>
-                    </li>
-                    <?php endforeach;?>
-                </ul>
-                <div id="myQuestion">
-                    <span class="error"></span>
-                    <span class="message"></span>
-                    <a href="" class="btn-30">
+                <div class="answer">
+                    <?php echo $row->answer;?>
+                </div>
+            </li>
+            <?php endforeach;?>
+        </ul>
+
+        <a href="" class="btn-30">
                         <span class="btn-30-txt">
                             <?php echo Yii::t('faq', 'Задать свой вопрос');?>
                         </span>
-                    </a>
+        </a>
 
-                    <div id="myQuestion-form">
-                        <form action="" method="post" id="set_question">
+        <div class="row">
+            <div class="seven columns">
+                <form action="" method="post" id="set_question">
+                    <?php echo CHtml::activeTextArea($form, 'question',
+                    array(
+                        'rows' => 3,
+                        'class' => 'txtarea',
+                        'placeholder' => Yii::t('faq', 'Вопрос'),
+                    )); ?>
 
-                            <?php echo CHtml::activeTextArea($form, 'question',
+                    <div class="row">
+                        <div class="five columns">
+                            <?php echo CHtml::activeTextField($form, 'name',
                             array(
-                                'rows' => 3,
-                                'class' => 'txtarea',
-                                'placeholder' => Yii::t('faq', 'Вопрос'),
+                                'class' => 'txt',
+                                'placeholder' => Yii::t('faq', 'Имя'),
                             )); ?>
-
-                            <div class="clear"></div>
-                            <div class="txt-form name">
-                                <div class="txt-form-cl">
-                                    <?php echo CHtml::activeTextField($form, 'name',
-                                    array(
-                                        'class' => 'txt',
-                                        'placeholder' => Yii::t('faq', 'Имя'),
-                                    )); ?>
-                                </div>
-                            </div>
-                            <div class="txt-form mail">
-                                <div class="txt-form-cl">
-                                    <?php echo CHtml::activeTextField($form, 'email',
-                                    array(
-                                        'class' => 'txt',
-                                        'placeholder' => Yii::t('faq', 'Email'),
-                                    )); ?>
-                                </div>
-                            </div>
-                            <div class="clear"></div>
-                            <div class="round-btn">
-                                <div class="round-btn-cl">
-                                    <input type="submit"
-                                           value="<?php echo Yii::t('faq', 'Отправить');?>"/>
-                                </div>
-                            </div>
-                        </form>
+                        </div>
+                        <div class="seven columns">
+                            <?php echo CHtml::activeTextField($form, 'email',
+                            array(
+                                'class' => 'txt',
+                                'placeholder' => Yii::t('faq', 'Email'),
+                            )); ?>
+                        </div>
 
                     </div>
-                </div>
-            </div>
+                </form>
 
-            <div class="clear"></div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="seven columns">
+                <form action="" method="post" id="set_question">
+
+
+
+                    <div class="row">
+                        <div class="three columns">
+
+                        </div>
+                        <div class="four columns">
+
+                        </div>
+                    </div>
+                    <input type="submit"
+                           value="<?php echo Yii::t('faq', 'Отправить');?>"/>
+                </form>
+            </div>
         </div>
 
+
     </div>
-    <script type="text/javascript">
-        var ACCORDION_MODE = false;
-        $(document).ready(function () {
-            $('#blockFAQ>ul>li>a').click(function () {
-                th = $(this).parent();
-                if (ACCORDION_MODE && !th.hasClass('active')) {
-                    th.parent().find('>li>div.answer').slideUp(300);
-                    th.parent().find('>li').removeClass('active');
-                }
-                th.find('>div.answer').slideToggle(300, function () {
-                    th.toggleClass('active');
-                });
-                return false;
-            });
-        });
-
-        $(document).ready(function () {
-            $('#myQuestion a').click(function () {
-                $('#myQuestion-form').show();
-
-                $('#myQuestion a.btn-30').hide();
-                return false;
-            });
-        });
-
-        $(document).ready(function () {
-            var options = {
-                success:showsQuestionResponse,
-                clearForm:false,
-                url:'/ajax/setQuestion/'
-            };
-
-            $('#set_question').submit(function () {
-                $(this).ajaxSubmit(options);
-                $('#myQuestion-form span.error').hide();
-                return false;
-            });
-        });
-
-        function showsQuestionResponse(responseText) {
-            if (responseText == 0) {
-
-                $('span.error').html('<?php echo Yii::t('faq', 'Необходимо корректно заполнить все поля.');?>' + '<br /><br />');
+</div>
+<script type="text/javascript">
+    var ACCORDION_MODE = false;
+    $(document).ready(function () {
+        $('.faq>ul>li>a').click(function () {
+            th = $(this).parent();
+            if (ACCORDION_MODE && !th.hasClass('active')) {
+                th.parent().find('>li>div.answer').slideUp(300);
+                th.parent().find('>li').removeClass('active');
             }
-            else if (responseText == 1) {
-                $('#myQuestion a.btn-30').show();
-                $('#myQuestion-form').hide();
-                $('span.message').html('<?php echo Yii::t('account', 'Ваш вопрос отправлен.')?>'+ '<br /><br />');
+            th.find('>div.answer').slideToggle(300, function () {
+                th.toggleClass('active');
+            });
+            return false;
+        });
+    });
 
-                setTimeout(function () {
-                    $('span.message').hide();
+    $(document).ready(function () {
+        $('#myQuestion a').click(function () {
+            $('#myQuestion-form').show();
 
-                }, 3000);
-            }
+            $('#myQuestion a.btn-30').hide();
+            return false;
+        });
+    });
 
+    $(document).ready(function () {
+        var options = {
+            success:showsQuestionResponse,
+            clearForm:false,
+            url:'/ajax/setQuestion/'
+        };
+
+        $('#set_question').submit(function () {
+            $(this).ajaxSubmit(options);
+            $('#myQuestion-form span.error').hide();
+            return false;
+        });
+    });
+
+    function showsQuestionResponse(responseText) {
+        if (responseText == 0) {
+
+            $('span.error').html('<?php echo Yii::t('faq', 'Необходимо корректно заполнить все поля.');?>' + '<br /><br />');
         }
-    </script>
+        else if (responseText == 1) {
+            $('#myQuestion a.btn-30').show();
+            $('#myQuestion-form').hide();
+            $('span.message').html('<?php echo Yii::t('account', 'Ваш вопрос отправлен.')?>' + '<br /><br />');
+
+            setTimeout(function () {
+                $('span.message').hide();
+
+            }, 3000);
+        }
+
+    }
+</script>
--- themes/mobispot/views/pages/index.php
+++ themes/mobispot/views/pages/index.php
@@ -1,18 +1,14 @@
-<div id="main-container">
-    <div id="cont-block" class="center">
-        <div id="cont-block-900" class="center">
-            <div id="zag-cont-block"><?php echo $model->title;?></div>
+<div class="page">
+    <div class="title"><?php echo $model->title;?></div>
 
-            <div id="blockPage">
-                <?php if (!empty($model->menu)):?>
-                <div id="page_menu">
-                    <?php echo $model->body;?>
-                </div>
-                <?php endif;?>
-                <?php echo $model->body;?>
-            </div>
-
-            <div class="clear"></div>
+    <div class="content">
+        <?php if (!empty($model->menu)): ?>
+        <div id="page_menu">
+            <?php echo $model->body;?>
         </div>
-
+        <?php endif;?>
+        <?php echo $model->body;?>
     </div>
+
+
+</div>
