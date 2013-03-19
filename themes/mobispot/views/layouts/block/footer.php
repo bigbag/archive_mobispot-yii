6f983e6a508ee0d2f896120923d37da228d0fe9f
Lyashkov Pavel <bilbo.kem@gmail.com>
2013-03-19 17:27:16 +0400

    fix spots lixt css

--- themes/mobispot/views/layouts/block/footer.php
+++ themes/mobispot/views/layouts/block/footer.php
@@ -5,7 +5,7 @@
       <?php $allLang=Lang::getLangArray();?>
       <ul class="lang-dropdown">
         <li class="dropdown toggle-active">
-          <span class="spot-button"><?php echo $allLang[Yii::app()->language];?></span>
+          <a class="spot-button" href="javascript:;"><?php echo $allLang[Yii::app()->language];?></a>
           <ul class="options">
             <?php foreach ($allLang as $key=>$value):?>
             <?php if($key!=Yii::app()->language):?>
--- themes/mobispot/views/user/block/spots.php
+++ themes/mobispot/views/user/block/spots.php
@@ -1,25 +1,6 @@
-<li id="<?php echo $data->discodes_id;?>" class="bg-gray toggle-box onlyOpen">
+<li id="<?php echo $data->discodes_id;?>" class="spot-content_li">
   <h3><?php echo $data->name?></h3>
-    <div id="<?php echo $data->discodes_id;?>Form" class="spot-content slide-content">
-      <div class="spot-content_row"><a class="settings-button spot-button right text-center" href="javascript:;">Settings</a></div>
-        <div class="spot-content_row">
-          <div class="spot-item">
-            <textarea></textarea>
-            <label class="text-center label-cover">
-              <h4>Drag your files here or begin to type info or links</h4>
-              <span>A maximum file size limit of 25mb for free accounts</span>
-            </label>
-          </div>
-        </div>
-        <div class="spot-content_row spot-options add-active">
-          <a class="radio-link active" href="javascript:;">
-            <i class="large"></i>
-            Allow download spot as a card
-          </a>
-          <a class="radio-link" href="javascript:;">
-            <i class="large"></i>
-            Make it private
-          </a>
-        </div>
+    <div id="<?php echo $data->discodes_id;?>_form" class="spot-content">
+
     </div>
 </li>
