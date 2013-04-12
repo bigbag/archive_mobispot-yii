8e96baacfbd916cd19b07dd987210acfd43f22b5
Lyashkov Pavel <bilbo.kem@gmail.com>
2013-04-12 19:14:01 +0400

    add google analytics code

--- themes/mobispot/views/layouts/block/script/all.php
+++ themes/mobispot/views/layouts/block/script/all.php
@@ -13,3 +13,16 @@
 <script src="/themes/mobispot/javascripts/angular/app/controllers.js"></script>
 <script src="/themes/mobispot/javascripts/angular/app/filters.js"></script>
 <script src="/themes/mobispot/javascripts/angular/app/directives.js"></script>
+
+<script type="text/javascript">
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-33307941-1']);
+  _gaq.push(['_setDomainName', 'mobispot.com']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>
