f1acbf950107532b474cbe6b7ab2b366690d6945
git <dionicii@mail.ru>
2013-05-23 12:42:41 +0300

    Visibility of Bind soc net button fix

--- themes/mobispot/views/widget/spot/personal/new_text.php
+++ themes/mobispot/views/widget/spot/personal/new_text.php
@@ -3,7 +3,9 @@
     <p class="item-area item-type__text"><?php echo CHtml::encode($content)?></p>
     <div class="spot-cover slow" ui-event="{dblclick : 'editContent(spot, <?php echo $key;?>, $event)'}">
       <div class="spot-activity">
-        <a class="button bind-spot round" ng-click="bindSocial(spot, <?php echo $key;?>, $event)">&#xe005;</a>
+	    <?php if (SocInfo::isSocLink(CHtml::encode($content))):?>
+	    <a class="button bind-spot round" ng-click="bindSocial(spot, <?php echo $key;?>, $event)">&#xe005;</a>
+	    <?php endif;?>
         <a class="button edit-spot round" ng-click="editContent(spot, <?php echo $key;?>, $event)">&#xe009;</a>
         <a class="button remove-spot round" ng-click="removeContent(spot, <?php echo $key;?>, $event)">&#xe00b;</a>
       </div>
