197dae5650e7c46b013bec3a491b27ed2c8032ee
Denis <dionicii@mail.ru>
2013-06-18 18:59:51 +0400

    Instagram without media

--- protected/extensions/eauth/custom_services/CustomInstagramOAuthService.php
+++ protected/extensions/eauth/custom_services/CustomInstagramOAuthService.php
@@ -3,62 +3,79 @@
 require_once dirname(dirname(__FILE__)).'/EOAuth2Service.php';
 
 class CustomInstagramOAuthService extends EOAuth2Service {	
-	
-	protected $name = 'instagram';
-	protected $title = 'Instagram';
-	protected $type = 'OAuth';
-	protected $jsArguments = array('popup' => array('width' => 500, 'height' => 450));
+  protected $name = 'instagram';
+  protected $title = 'Instagram';
+  protected $type = 'OAuth';
+  protected $jsArguments = array('popup' => array('width' => 500, 'height' => 450));
 
-	protected $client_id = '';
-	protected $client_secret = '';
-	protected $scope = 'basic';
-	protected $providerOptions = array(
-		'authorize' => 'https://api.instagram.com/oauth/authorize/',
-		'access_token' => 'https://api.instagram.com/oauth/access_token',
+  protected $client_id = '';
+  protected $client_secret = '';
+  protected $scope = 'basic';
+  protected $providerOptions = array(
+    'authorize' => 'https://api.instagram.com/oauth/authorize/',
+    'access_token' => 'https://api.instagram.com/oauth/access_token',
+  );
+	
+  protected function fetchAttributes() {
+    $info = $this->makeSignedRequest('https://api.instagram.com/v1/users/self');
+	$this->attributes['id'] = $this->getState('idUser');
+	$this->attributes['name'] = $this->getState('username');
+	$info = $info->data;
+	if(!empty($info->username))
+	  $this->attributes['url'] = 'http://instagram.com/'.$info->username;
+	/*  
+	if(!empty($info->full_name))
+		$this->attributes['username'] = $info->full_name;
+	elseif(!empty($info->username))
+		$this->attributes['username'] = $info->username;
+	$this->attributes['url'] = 'http://instagram.com/'.$info->username;			
+	$this->attributes['photo'] = $info->profile_picture;
+	if(!empty($info->bio))
+		$this->attributes['about'] = $info->bio;				
+	if(!empty($info->website))
+		$this->attributes['url'] = $info->website;
+	
+    $media = $this->makeSignedRequest('https://api.instagram.com/v1/users/'.$this->getState('idUser').'/media/recent');
+    if(!empty($media->data[0])){
+      $media = $media->data[0];
+	  $this->attributes['last_status'] = '<a href="'.$media->link.'" target="_blank">'.$media->caption->text.'<br/><img src="'.$media->images->standard_resolution->url.'"/></a>';
+    }
+	*/
+  }
+/*
+  protected function getAccessToken($code) {
+	$params = array(
+		'client_id' => $this->client_id,
+		'client_secret' => $this->client_secret,
+		'grant_type' => 'authorization_code',
+		'code' => $code,
+		'redirect_uri' => $this->getState('redirect_uri'),
 	);
-	
-	protected function fetchAttributes() {
-		$info = $this->makeSignedRequest('https://api.instagram.com/v1/users/self');
-		$this->attributes['id'] = $this->getState('idUser');
-		$this->attributes['name'] = $this->getState('username');
-		$info = $info->data;
-		if(!empty($info->full_name))
-			$this->attributes['username'] = $info->full_name;
-		else
-			$this->attributes['username'] = $info->username;
-		$this->attributes['url'] = 'http://instagram.com/'.$info->username;			
-		$this->attributes['photo'] = $info->profile_picture;
-		if(!empty($info->bio))
-			$this->attributes['about'] = $info->bio;				
-		if(!empty($info->website))
-			$this->attributes['url'] = $info->website;
-			
-		$this->attributes['token'] = $this->getState('auth_token');
-		
-		$media = $this->makeSignedRequest('https://api.instagram.com/v1/users/'.$this->getState('idUser').'/media/recent');
-		if(!empty($media->data[0])){
-			$media = $media->data[0];
-			$this->attributes['last_status'] = '<a href="'.$media->link.'" target="_blank">'.$media->caption->text.'<br/><img src="'.$media->images->standard_resolution->url.'"/></a>';
-		}
-	}
+	return $this->makeRequest($this->getTokenUrl($code), array('data' => $params));
+  }	
+ */ 
+ 
+  protected function getCodeUrl($redirect_uri){
+    $this->setState('instagram_redirect_uri', urlencode($redirect_uri));
+    $url = parent::getCodeUrl($redirect_uri);
+	return $url;
+  }
+  
+  protected function getTokenUrl($code)
+  {
+    return $this->providerOptions['access_token'];
+  }
 
-	protected function getAccessToken($code) {
-		$params = array(
-			'client_id' => $this->client_id,
-			'client_secret' => $this->client_secret,
-			'grant_type' => 'authorization_code',
-			'code' => $code,
-			'redirect_uri' => $this->getState('redirect_uri'),
-		);
-		return $this->makeRequest($this->getTokenUrl($code), array('data' => $params));
-	}	
-	
-	protected function saveAccessToken($token) {
-		$this->setState('auth_token', $token->access_token);
-		$this->setState('idUser', $token->user->id);
-		$this->setState('username', $token->user->username);
-		$this->access_token = $token->access_token;
-	}	
+    protected function getAccessToken($code)
+    {
+        return $this->makeRequest($this->getTokenUrl($code), array('data'=>'client_id=' . $this->client_id . '&client_secret=' . $this->client_secret . '&grant_type=authorization_code&redirect_uri='.$this->getState('instagram_redirect_uri').'&code=' . $code));
+    }
 
-	
+  
+  protected function saveAccessToken($token) {
+	$this->setState('auth_token', $token->access_token);
+	$this->setState('idUser', $token->user->id);
+	$this->setState('username', $token->user->username);
+	$this->access_token = $token->access_token;
+  }	
 }
--- protected/models/SocInfo.php
+++ protected/models/SocInfo.php
@@ -124,16 +124,16 @@ class SocInfo extends CFormModel
     $net['note'] = Yii::t('eauth', '');
     $net['smallIcon'] = 'youtube16.png';
     $socNetworks[] = $net;
-/*
-    $net['name'] = 'Instagram';
+
+    $net['name'] = 'instagram';
     $net['baseUrl'] = 'instagram.com';
-    $net['invite'] = Yii::t('eauth', '');
+    $net['invite'] = Yii::t('eauth', 'Watch more on');
     $net['inviteClass'] = 'i-soc_ing';
 	$net['inviteValue'] = '&#xe006;';
     $net['note'] =  Yii::t('eauth', '');
-    $net['smallIcon'] = '';
+    $net['smallIcon'] = 'instagram16.png';
     $socNetworks[] = $net;
-*/
+
     return $socNetworks;
   }
 
@@ -1008,36 +1008,19 @@ class SocInfo extends CFormModel
 			}
 		}
 
-      }elseif($socNet == 'Instagram'){
-        if(!isset(Yii::app()->user->token) || (Yii::app()->user->service != 'instagram'))
-          $this->userDetail['soc_username'] = Yii::t('eauth','Сначала авторизуйтесь черз Ваш профиль в Instagram');
-        else{
-        /*
-          //Parce html
-          $ch = curl_init();
-          curl_setopt($ch, CURLOPT_URL, 'http://instagram.com/'.$socUsername);
-          curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-          curl_setopt($ch, CURLOPT_CAINFO, dirname(__FILE__).'/../config/ca-bundle.crt');
-          $profile = curl_exec($ch);
-          $headers = curl_getinfo($ch);
-          curl_close($ch);
-          $profile = substr($profile, (strpos($profile, ('"username":'.$socUsername))));
-          $match = array();
-          preg_match('~"id":"[0-9]+"~', $profile, $match);
-          if(isset($match[0])){
-            $socUsername = str_replace('"id":"', '', $match[0]);
-            $socUsername = str_replace('"', '', $socUsername);
-          }
-        */
-          $socUser = $this->makeRequest('https://api.instagram.com/v1/users/search?q='.$socUsername.'&count=1&access_token='.Yii::app()->user->token);
-          if(!is_string($socUser)){
+      }elseif($socNet == 'instagram'){
+        $socUser = $this->makeRequest('https://api.instagram.com/v1/users/search?q='.$socUsername.'&count=1&client_id='.Yii::app()->eauth->services['instagram']['client_id']);
+        if(!is_string($socUser) && isset($socUser['data']) && isset($socUser['data'][0])){
             $socUser = $socUser['data'][0];
             if(!empty($socUser['full_name']))
               $this->userDetail['soc_username'] = $socUser['full_name'];
-            else
+            elseif(!empty($socUser['username']))
               $this->userDetail['soc_username'] = $socUser['username'];
-            $this->userDetail['soc_url'] = 'http://instagram.com/'.$socUser['username'];
-            $this->userDetail['photo'] = $socUser['profile_picture'];
+			if(!empty($socUser['username']))
+              $this->userDetail['soc_url'] = 'http://instagram.com/'.$socUser['username'];
+			if(!empty($socUser['profile_picture']))
+              $this->userDetail['photo'] = $socUser['profile_picture'];
+			/*  
             if(!empty($socUser['bio']))
               $this->userDetail['about'] = $socUser['bio'];
             if(!empty($socUser['website']))
@@ -1048,9 +1031,9 @@ class SocInfo extends CFormModel
               $media = $media['data'][0];
               $this->userDetail['last_status'] = '<a href="'.$media['link'].'" target="_blank">'.$media['caption']['text'].'<br/><img src="'.$media['images']['standard_resolution']['url'].'"/></a>';
             }
+			*/
           }else
             $this->userDetail['soc_username'] = Yii::t('eauth','Пользователя с таким именем не существует:').$socUsername;
-        }
       }else{
         $this->userDetail['soc_username'] =  Yii::t('eauth', 'Социальная сеть не поддерживается: ').$socNet;
       }
@@ -1179,7 +1162,7 @@ class SocInfo extends CFormModel
         $username = substr($username, (strpos($username, 'youtube.com/user/') + 17));
       $username = $this->rmGetParam($username);
 	*/
-    }elseif($socNet == 'Instagram'){
+    }elseif($socNet == 'instagram'){
       if(strpos($username, 'instagram.com/') !== false)
         $username = substr($username, (strpos($username, 'instagram.com/') + 14));
       $username = $this->rmGetParam($username);
