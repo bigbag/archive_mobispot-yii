6a4bc996185733320f116e4d869120ab82bcad15
Denis <dionicii@mail.ru>
2013-06-24 15:14:48 +0400

    LinkedIn

--- index.php
+++ index.php
@@ -12,9 +12,9 @@ $yii = dirname(__FILE__) . '/yii/yiilite.php';
 $config = dirname(__FILE__) . '/protected/config/main.php';
 
 // remove the following lines when in production mode
-define('YII_DEBUG', true);
+define('YII_DEBUG', false);
 // specify how many levels of call stack should be shown in each log message
-define('YII_TRACE_LEVEL', 3);
+//define('YII_TRACE_LEVEL', 3);
 
 require_once($yii);
 Yii::createWebApplication($config)->run();
