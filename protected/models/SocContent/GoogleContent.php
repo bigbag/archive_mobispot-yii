2ff0e189495a11d1dcd9aa3c9dc5d726b6e3c2fb
Denis <dionicii@mail.ru>
2013-07-31 14:30:21 +0400

    refractor Google binging

--- protected/models/SocContent/GoogleContent.php
+++ protected/models/SocContent/GoogleContent.php
@@ -21,6 +21,136 @@ class GoogleContent extends SocContentBase
         return $result;
     }
 
+    public static function getContent($link, $discodesId = null, $dataKey = null)
+    {
+        $userDetail = array();
+        $socUsername = self::parseUsername($link);
+        
+        $url = 'https://www.googleapis.com/plus/v1/people/' . $socUsername . '?key=' . Yii::app()->eauth->services['google_oauth']['key'];
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_CAINFO, Yii::app()->eauth->services['ssl']['path']);
+        //curl_setopt($ch, CURLOPT_CAINFO, dirname(__FILE__).'/../config/ca-bundle.crt');
+
+        $curl_result = curl_exec($ch);
+        curl_close($ch);
+
+        $socUser = CJSON::decode($curl_result, true);
+
+        if (!isset($socUser['error']) && isset($socUser['id']))
+        {
+            $userDetail['UserExists'] = true;
+            if (isset($socUser['displayName']))
+                $userDetail['soc_username'] = $socUser['displayName'];
+            if (isset($socUser['image']) && isset($socUser['image']['url']))
+                $userDetail['photo'] = $socUser['image']['url'];
+            if (isset($socUser['name']) && isset($socUser['name']['givenName']))
+                $userDetail['first_name'] = $socUser['name']['givenName'];
+            if (isset($socUser['name']) && isset($socUser['name']['familyName']))
+                $userDetail['last_name'] = $socUser['name']['familyName'];
+            /*
+              if(isset($socUser['gender']))
+              $userDetail['gender'] = $socUser['gender'];
+              if(isset($socUser['placesLived'])){
+              foreach($socUser['placesLived'] as $place){
+              if(isset($place['primary']) && ($place['primary']==true))
+              $userDetail['location'] = $place['value'];
+              }
+              }
+              if(isset($socUser['organizations'])){
+              foreach($socUser['organizations'] as $org){
+              if(isset($org['primary']) && ($org['primary']==true)){
+              if($org['type'] == 'work')
+              $userDetail['work'] = $org['name'];
+              else if($org['type'] == 'school')
+              $userDetail['school'] = $org['name'];
+              }
+              }
+
+              }
+              if(isset($socUser['aboutMe']))
+              $userDetail['about'] = $socUser['aboutMe'];
+              if(isset($socUser['url']))
+              $userDetail['soc_url'] = $socUser['url'];
+             */
+            //Последний пост
+            $url = 'https://www.googleapis.com/plus/v1/people/' . $socUsername . '/activities/public?key=' . Yii::app()->eauth->services['google_oauth']['key'];
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_CAINFO, Yii::app()->eauth->services['ssl']['path']);
+
+            $curl_result = curl_exec($ch);
+            curl_close($ch);
+
+            //$userFeed = CJSON::decode($curl_result, true);
+            $userFeed = json_decode($curl_result, true);
+
+            unset($lastPost);
+            $i = 0;
+            $prevPageUrl = '';
+
+            while (!isset($lastPost))
+            {
+                if (isset($userFeed['items']) && isset($userFeed['items'][$i]) && isset($userFeed['items'][$i]['actor']) && isset($userFeed['items'][$i]['actor']['id']) && ($userFeed['items'][$i]['actor']['id'] == $socUser['id']) && isset($userFeed['items'][$i]['object']))
+                {
+                    $lastPost = $userFeed['items'][$i];
+                }
+                elseif (!isset($userFeed['items']) || (!isset($userFeed['items'][$i])))
+                {
+                    $lastPost = 'no';
+                }
+                else
+                {
+                    $i++;
+                }
+            }
+
+            if ($lastPost != 'no')
+            {
+                if (isset($lastPost['object']['content']))
+                    $userDetail['last_status'] = $lastPost['object']['content'];
+                //Картинка
+                if (isset($lastPost['object']['attachments']))
+                {
+                    $i = 0;
+                    unset($imgSrc);
+                    while (isset($lastPost['object']['attachments'][$i]) && !isset($imgSrc))
+                    {
+                        if (isset($lastPost['object']['attachments'][$i]['image']) && isset($lastPost['object']['attachments'][$i]['image']['url']) && (strlen($lastPost['object']['attachments'][$i]['image']['url']) > 0) && ($lastPost['object']['attachments'][$i]['image']['url'] != ' https:'))
+                        {
+                            $imgSrc = $lastPost['object']['attachments'][$i]['image']['url'];
+                        }
+                        else
+                            $i++;
+                    }
+                    if (isset($imgSrc))
+                    {
+                        $userDetail['last_img'] = $imgSrc;
+                        if (isset($lastPost['object']['attachments'][$i]['displayName']) && (strlen($lastPost['object']['attachments'][$i]['displayName']) > 0))
+                            $userDetail['last_img_msg'] = $lastPost['object']['attachments'][$i]['displayName'];
+                        elseif (isset($lastPost['object']['attachments'][$i]['content']) && (strlen($lastPost['object']['attachments'][$i]['content']) > 0))
+                            $userDetail['last_img_msg'] = $lastPost['object']['attachments'][$i]['content'];
+                        elseif (isset($userDetail['last_status']))
+                        {
+                            $userDetail['last_img_msg'] = $userDetail['last_status'];
+                            unset($userDetail['last_status']);
+                        }
+                        if (isset($lastPost['object']['attachments'][$i]['url']))
+                            $userDetail['last_img_href'] = $lastPost['object']['attachments'][$i]['url'];
+                    }
+                }
+            }
+        }else
+        {
+            $userDetail['error'] = Yii::t('eauth', "This account doesn't exist:") . $socUsername;
+        }
+    
+        return $userDetail;
+    }
+    
     public static function parseUsername($link)
     {
         $username = $link;
