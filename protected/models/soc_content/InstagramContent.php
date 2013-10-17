1cf4d39f3dcc6bba7eddfa1537ce6938eba9d974
Denis <dionicii@mail.ru>
2013-10-17 12:55:27 +0400

    feature привязка контента Instagram

--- protected/models/soc_content/InstagramContent.php
+++ protected/models/soc_content/InstagramContent.php
@@ -5,13 +5,16 @@ class InstagramContent extends SocContentBase
 
     public static function isLinkCorrect($link, $discodesId = null, $dataKey = null)
     {
-        $socUsername = self::parseUsername($link);
         $result = 'ok';
 
-        $socUser = self::makeRequest('https://api.instagram.com/v1/users/search?q=' . $socUsername . '&count=1&client_id=' . Yii::app()->eauth->services['instagram']['client_id']);
-        if (is_string($socUser) || !isset($socUser['data']) || !isset($socUser['data'][0]))
+        if (!(strpos($link, 'instagram.com') !== false && (strpos($link, '/p/') !== false) && strlen($link) > (strpos($link, '/p/') + strlen('/p/'))))
         {
-            $result = Yii::t('eauth', "This account doesn't exist:") . $socUsername;
+            $socUsername = self::parseUsername($link);
+            $socUser = self::makeRequest('https://api.instagram.com/v1/users/search?q=' . $socUsername . '&count=1&client_id=' . Yii::app()->eauth->services['instagram']['client_id']);
+            if (is_string($socUser) || !isset($socUser['data']) || !isset($socUser['data'][0]))
+            {
+                $result = Yii::t('eauth', "This account doesn't exist:") . $socUsername;
+            }
         }
 
         return $result;
@@ -30,100 +33,118 @@ class InstagramContent extends SocContentBase
     public static function getContent($link, $discodesId = null, $dataKey = null)
     {
         $userDetail = array();
-        $socUsername = self::parseUsername($link);
-
-        $socUser = self::makeRequest('https://api.instagram.com/v1/users/search?q=' . $socUsername . '&count=1&client_id=' . Yii::app()->eauth->services['instagram']['client_id']);
-        if (!is_string($socUser) && isset($socUser['data']) && isset($socUser['data'][0]))
+        unset($post);
+        
+        if (strpos($link, 'instagram.com') !== false && (strpos($link, '/p/') !== false) && strlen($link) > (strpos($link, '/p/') + strlen('/p/')))
         {
-            $socUser = $socUser['data'][0];
-            /* if(!empty($socUser['full_name']))
-              $userDetail['soc_username'] = $socUser['full_name'];
-              elseif(!empty($socUser['username']))
-              $userDetail['soc_username'] = $socUser['username'];
-             */
-            if (!empty($socUser['username']))
-                $userDetail['soc_url'] = 'http://instagram.com/' . $socUser['username'];
-            /*  if(!empty($socUser['profile_picture']))
-              $userDetail['photo'] = $socUser['profile_picture'];
-             */
-
-            $techSoc = SocToken::model()->findByAttributes(array(
-                'type' => 10,
-                'is_tech' => true
-            ));
-
-            if ($techSoc && isset($socUser['id']))
+            //привязан пост
+            $slug = substr($link, (strpos($link, '/p/') + strlen('/p/')));
+            $slug = self::rmGetParam($slug);
+            
+            $postMeta = self::makeRequest('http://api.instagram.com/oembed?url=' . $link);
+            if (!empty($postMeta['media_id']))
             {
-                $media = self::makeRequest('https://api.instagram.com/v1/users/' . $socUser['id'] . '/media/recent?count=1&access_token=' . $techSoc->user_token);
-
-                if (isset($media['data']) && isset($media['data'][0]))
-                {
-Yii::app()->session['debug'] = print_r($media['data'][0], true);
-                    if (!empty($media['data'][0]['user']['profile_picture']))
-                        $userDetail['photo'] = $media['data'][0]['user']['profile_picture'];
-                    if (!empty($media['data'][0]['user']['full_name']))
-                        $userDetail['soc_username'] = $media['data'][0]['user']['full_name'];
-                    elseif (!empty($media['data'][0]['user']['username']))
-                        $userDetail['soc_username'] = $media['data'][0]['user']['username'];
-                    if (isset($media['data'][0]['location']) && !empty($media['data'][0]['location']['name']))
-                        $userDetail['sub-line'] = '<span class="icon">&#xe018;</span>' . $media['data'][0]['location']['name'];
-                    if (!empty($media['data'][0]['created_time']))
-                    {
-                        $dateDiff = time() - (int)$media['data'][0]['created_time'];
-                        $userDetail['sub-time'] = SocContentBase::timeDiff($dateDiff);
-                    }
-
-                    if (isset($media['data'][0]['images']) && isset($media['data'][0]['images']['standard_resolution']) && !empty($media['data'][0]['images']['standard_resolution']['url']))
-                        $userDetail['last_img'] = $media['data'][0]['images']['standard_resolution']['url'];
-                    elseif (isset($media['data'][0]['images']) && isset($media['data'][0]['images']['thumbnail']) && !empty($media['data'][0]['images']['thumbnail']['url']))
-                        $userDetail['last_img'] = $media['data'][0]['images']['thumbnail']['url'];
-                    if (isset($media['data'][0]['caption']) && !empty($media['data'][0]['caption']['text']))
-                        $userDetail['last_img_msg'] = $media['data'][0]['caption']['text'];
-                    if (!empty($media['data'][0]['link']))
-                        $userDetail['last_img_href'] = $media['data'][0]['link'];
-                    if (isset($media['data'][0]['likes']) && isset($media['data'][0]['likes']['count']) && isset($media['data'][0]['likes']['data']) && isset($media['data'][0]['likes']['data'][0]) && !empty($media['data'][0]['likes']['data'][0]['username']) && !empty($media['data'][0]['likes']['data'][0]['full_name']))
-                    {
-                        $userDetail['likes-block'] = '<a class="authot-name" href="http://instagram.com/' . $media['data'][0]['likes']['data'][0]['username'] . '">' . $media['data'][0]['likes']['data'][0]['full_name'] . '</a>';
-                        
-                        if (isset($media['data'][0]['likes']['data'][1]) && !empty($media['data'][0]['likes']['data'][1]['username']) && !empty($media['data'][0]['likes']['data'][1]['full_name']))
-                        {
-                            if ($media['data'][0]['likes']['count'] > 2)
-                                $userDetail['likes-block'] .= ', ';
-                            else
-                                $userDetail['likes-block'] .= ' '. Yii::t('eauth', 'and') . ' ';
-                            $userDetail['likes-block'] .= '<a class="authot-name" href="http://instagram.com/' . $media['data'][0]['likes']['data'][1]['username'] . '">' . $media['data'][0]['likes']['data'][1]['full_name'] . '</a>';
-                            if ($media['data'][0]['likes']['count'] > 2)
-                                $userDetail['likes-block'] .= ' '. Yii::t('eauth', 'and') . ' <b>' . ($media['data'][0]['likes']['count'] - 2) . '</b> ' . Yii::t('eauth', 'others');
-                        }
-                        $userDetail['likes-block'] .= ' '.Yii::t('eauth', 'like this') . '.';
-                    }
-                }
+                $postJSON = self::makeRequest('https://api.instagram.com/v1/media/' 
+                    . $postMeta['media_id'] 
+                    . '?client_id='
+                    . Yii::app()->eauth->services['instagram']['client_id']);
+                if (isset($postJSON['data']) && !empty($postJSON['data']['id']))
+                    $post = $postJSON['data'];
             }
-            /*
-              $user=User::model()->findByAttributes(array(
-              'instagram_id'=>$socUser['id']
-              ));
-              if($user && strlen($user->instagram_media_id > 0)){
-              $media = self::makeRequest('https://api.instagram.com/v1/media/'.$user->instagram_media_id.'?client_id='.Yii::app()->eauth->services['instagram']['client_id']);
-              if(isset($media['data'])){
-              if(isset($media['data']['images']) && isset($media['data']['images']['standard_resolution']) && !empty($media['data']['images']['standard_resolution']['url']))
-              $userDetail['last_img'] = $media['data']['images']['standard_resolution']['url'];
-              elseif(isset($media['data']['images']) && isset($media['data']['images']['thumbnail']) && !empty($media['data']['images']['thumbnail']['url']))
-              $userDetail['last_img'] = $media['data']['images']['thumbnail']['url'];
-              if(isset($media['data']['caption']) && !empty($media['data']['caption']['text']))
-              $userDetail['last_img_msg'] = $media['data']['caption']['text'];
-              if(!empty($media['data']['link']))
-              $userDetail['last_img_href'] = $media['data']['link'];
-              }
-              }
-             */
         }
         else
-            $userDetail['soc_username'] = Yii::t('eauth', "This account doesn't exist:") . $socUsername;
+        {
+            //привязан профиль
+            $socUsername = self::parseUsername($link);
 
+            $socUser = self::makeRequest('https://api.instagram.com/v1/users/search?q=' . $socUsername . '&count=1&client_id=' . Yii::app()->eauth->services['instagram']['client_id']);
+            if (!is_string($socUser) && isset($socUser['data']) && isset($socUser['data'][0]))
+            {
+                $socUser = $socUser['data'][0];
+                if (!empty($socUser['username']))
+                    $userDetail['soc_url'] = 'http://instagram.com/' . $socUser['username'];
+
+                $techSoc = SocToken::model()->findByAttributes(array(
+                    'type' => 10,
+                    'is_tech' => true
+                ));
+
+                if ($techSoc && isset($socUser['id']))
+                {
+                    $media = self::makeRequest('https://api.instagram.com/v1/users/' . $socUser['id'] . '/media/recent?count=1&access_token=' . $techSoc->user_token);
+                    if (isset($media['data']) && isset($media['data'][0]) && !empty($media['data'][0]['id']))
+                        $post = $media['data'][0];
+                }
+            }
+            else
+                $userDetail['soc_username'] = Yii::t('eauth', "This account doesn't exist:") . $socUsername;
+        }
+        
+        if (isset($post))
+        {
+            if (!empty($post['user']['profile_picture']))
+                $userDetail['photo'] = $post['user']['profile_picture'];
+            if (!empty($post['user']['full_name']))
+                $userDetail['soc_username'] = $post['user']['full_name'];
+            elseif (!empty($post['user']['username']))
+                $userDetail['soc_username'] = $post['user']['username'];
+            if (isset($post['location']) && !empty($post['location']['name']))
+                $userDetail['sub-line'] = '<span class="icon">&#xe018;</span>' . $post['location']['name'];
+            if (!empty($post['created_time']))
+            {
+                $dateDiff = time() - (int)$post['created_time'];
+                $userDetail['sub-time'] = SocContentBase::timeDiff($dateDiff);
+            }
+
+            if (isset($post['images']) && isset($post['images']['standard_resolution']) && !empty($post['images']['standard_resolution']['url']))
+                $userDetail['last_img'] = $post['images']['standard_resolution']['url'];
+            elseif (isset($post['images']) && isset($post['images']['thumbnail']) && !empty($post['images']['thumbnail']['url']))
+                $userDetail['last_img'] = $post['images']['thumbnail']['url'];
+            if (isset($post['caption']) && !empty($post['caption']['text']))
+                $userDetail['last_img_msg'] = $post['caption']['text'];
+            if (!empty($post['link']))
+                $userDetail['last_img_href'] = $post['link'];
+            if (isset($post['likes']) && isset($post['likes']['count']) && isset($post['likes']['data']) && isset($post['likes']['data'][0]) && !empty($post['likes']['data'][0]['username']) && !empty($post['likes']['data'][0]['full_name']))
+            {
+                $userDetail['likes-block'] = '<a class="authot-name" href="http://instagram.com/' . $post['likes']['data'][0]['username'] . '">' . $post['likes']['data'][0]['full_name'] . '</a>';
+                
+                if (isset($post['likes']['data'][1]) && !empty($post['likes']['data'][1]['username']) && !empty($post['likes']['data'][1]['full_name']))
+                {
+                    if ($post['likes']['count'] > 2)
+                        $userDetail['likes-block'] .= ', ';
+                    else
+                        $userDetail['likes-block'] .= ' '. Yii::t('eauth', 'and') . ' ';
+                    $userDetail['likes-block'] .= '<a class="authot-name" href="http://instagram.com/' . $post['likes']['data'][1]['username'] . '">' . $post['likes']['data'][1]['full_name'] . '</a>';
+                    if ($post['likes']['count'] > 2)
+                        $userDetail['likes-block'] .= ' '. Yii::t('eauth', 'and') . ' <b>' . ($post['likes']['count'] - 2) . '</b> ' . Yii::t('eauth', 'others');
+                }
+                $userDetail['likes-block'] .= ' '.Yii::t('eauth', 'like this') . '.';
+            }
+            
+            if (self::contentNeedSave($link))
+            {
+                if (!empty($userDetail['last_img']))
+                {
+                    $savedImg = self::saveImage($userDetail['last_img']);
+                    if ($savedImg)
+                        $userDetail['last_img'] = $savedImg;
+                }
+                $userDetail['soc_url'] = $link;
+            }
+        }
+                
         return $userDetail;
     }
-    
+   
+    public static function contentNeedSave($link)
+    {
+        $result = false;
+        if (strpos($link, 'instagram.com') !== false && (strpos($link, '/p/') !== false) && strlen($link) > (strpos($link, '/p/') + strlen('/p/')))
+            $result = true;
+
+        return $result;
+    }
+   
     public static function isLoggegByNet()
     {
         $answer = false;
