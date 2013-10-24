107cb483bb7f380bc67103efe2cb0cdfdcbd8120
Denis <dionicii@mail.ru>
2013-10-24 15:52:22 +0400

    fix фолловинг Tumblr

--- themes/mobile/js/controllers.js
+++ themes/mobile/js/controllers.js
@@ -3,10 +3,14 @@ function MobileCtrl($scope, $http) {
     $scope.followSocial = function(service, param, idBlock)
     {
         var data = {service: service, param:param, token:$scope.token};
-        $http.post('/mobile/spot/FollowSocial', data).success(function(data) {
+        var pathname = '/spot/';
+        if (window.location.pathname.toLowerCase().indexOf('/mobile/spot') != -1)
+            pathname = '/mobile/spot/';
+        $http.post((pathname + 'FollowSocial'), data).success(function(data) {
             if (!data.LoggedIn)
             {
-                window.location = 'http://' + window.location.hostname + '/mobile/spot/SocLogin?service=' + service 
+
+                window.location = 'http://' + window.location.hostname + pathname + 'SocLogin?service=' + service 
                                     + '&return_url=' 
                                     + escape(window.location.hostname + window.location.pathname + window.location.search) 
                                     + '&follow_param=' + escape(param);
