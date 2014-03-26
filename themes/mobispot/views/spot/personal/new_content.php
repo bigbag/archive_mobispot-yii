2e9053e30f02d305350b5885127d8b2ba0747ee7
Denis Amelin <dionicii@mail.ru>
2014-03-26 12:33:25 +0400

    fix передача ссылки на профиль в соцсети

--- themes/mobispot/views/spot/personal/new_content.php
+++ themes/mobispot/views/spot/personal/new_content.php
@@ -1,5 +1,6 @@
 <?php $dataKey = $key; ?>
 <?php $socContent = (array) $content; ?>
 <?php $socContent['dinamyc'] = true; ?>
+<?php $socContent['soc_url'] = $content['binded_link']; ?>
 <?php include(Yii::getPathOfAlias('webroot') . '/themes/mobispot/views/mobile/widget/spot/soc_content.php'); ?>
  
