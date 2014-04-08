d3b2fc66ae764e02625049ac6bfef589abfc6989
Denis Amelin <dionicii@mail.ru>
2014-04-08 10:48:06 +0400

    fix перевод на китайский упрощенный

--- protected/messages/zh_cn/general.php
+++ protected/messages/zh_cn/general.php
@@ -21,7 +21,7 @@ return array (
   'If you want your tickets close to hand,<br> put them on your wrist.' => '想把各种公共交通票都“戴”在你的手腕上吗?<br>智能手环帮你实现!',
   'Make a lasting<br> connection' => '建立联系',
   'Met someone?<br> Share your details instantly with Mobispot.<br> Like something?<br> Share it, and brands can share right back.' => '认识了新朋友?<br>轻松刷“环”,  与好友们分享自己的全部. <br>点赞了网页?<br>获得各种名牌的折扣和优惠.',
-  'One tap is<br> all it takes' => '轻轻툈触,<br>刷“环”过关',
+  'One tap is<br> all it takes' => '轻轻一触,<br>刷“环”过关',
   'Pay with a tap,<br> not with cash' => '轻轻一触,<br>刷“环”付款',
   'Save time, carry less. You can keep<br> your loyalty cards, tickets and<br> membership details on your Spot.' => '节省时间的好帮手 - 打折卡, 公车票,<br>出入证, 全部刷“环”, <br>无所不能!',
   'Spend less time paying for lunch,<br> spend more time eating it. Mobispot works<br> with your campus payment scheme.' => '便利的小金额付款方式, <br>智能手环让你足不出户, <br>轻松付款.',
