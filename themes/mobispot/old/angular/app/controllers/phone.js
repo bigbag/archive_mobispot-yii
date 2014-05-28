890eb885073bad4f4a9df53d1641d0a5e097c0cd
Lyashkov Pavel <bilbo.kem@gmail.com>
2014-05-28 18:46:20 +0400

    fix: css update

--- themes/mobispot/old/angular/app/controllers/phone.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-angular.module('mobispot').controller('PhonesCtrl', 
-  function($scope) {
-	$scope.initPhones = function(initValue)
-	{
-		$scope.parents = initValue;
-		var herher = $scope.parents.models;
-	}
-});
