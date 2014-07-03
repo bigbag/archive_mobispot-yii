9c20fadf0a4e312748a18a3ded6e462cebc15c47
Lyashkov Pavel <bilbo.kem@gmail.com>
2014-07-03 18:25:38 +0400

    fix: absolute url in twitter link

--- themes/mobispot/views/mobile/spot/personal/new_socnet.php
+++ themes/mobispot/views/mobile/spot/personal/new_socnet.php
@@ -2,7 +2,7 @@
 <article id="block-<?php echo $key;?>" class="spot-item" ng-init="loadSocContent(<?php echo $key;?>)">
     <div class="item-area type-itembox">
         <div class="item-head">
-            <a href="<?php echo CHtml::encode($content); ?>" class="type-link">
+            <a href="<?php echo echo YText::urlActivate(CHtml::encode($content)); ?>" class="type-link">
                 <img class="soc-icon" src="<?php echo MHttp::desktopHost(); ?>/themes/mobispot/socialmediaicons/<?php echo $socInfo->getSmallIcon(CHtml::encode($content));?>" height="18"> <span class="link"><?php echo CHtml::encode($content); ?></span>
             </a>
         </div>
