445305c1c027f29917bc38c43a4a521cc455d8b2
Lyashkov Pavel <bilbo.kem@gmail.com>
2014-07-07 17:26:22 +0400

    fix: pack mobile css and js files

--- themes/mobispot/views/layouts/mobile_view.php
+++ themes/mobispot/views/layouts/mobile_view.php
@@ -10,14 +10,13 @@
 
     <meta name="viewport" content="width=device-width" />
 
-    <link rel="stylesheet" href="/themes/mobile/stylesheets/style.css">
+    <link rel="stylesheet" href="/themes/mobile/stylesheets/style_view.min.css">
 
     <script src="https://maps.google.com/maps/api/js?key=<?php echo Yii::app()->eauth->services['google_oauth']['key']; ?>&sensor=true"></script>
     <script src="/themes/mobispot/js/jquery.min.js"></script>
     <script src="/themes/mobispot/js/angular.min.js"></script>
+
     <script src="/themes/mobile/js/mobile.js"></script>
-    <script src="/themes/mobile/js/app.js"></script>
-    <script src="/themes/mobile/js/controllers.js"></script>
 </head>
 <body>
     <div class="content-wrapper bg-gray">
@@ -31,5 +30,32 @@
          <span>Powered by</span> <img width="100" src="/themes/mobile/images/logo.png">
       </a>
   </footer>
+  <script>
+  angular.module('mobispot', [])
+    .controller('MobileCtrl',
+    function($scope, $http) {
+      $scope.followSocial = function(service, param, idBlock){
+        var data = {service: service, param:param, token:$scope.token};
+        var pathname = '/spot/';
+        if (window.location.pathname.toLowerCase().indexOf('/mobile/spot') != -1)
+            pathname = '/mobile/spot/';
+
+        $http.post((pathname + 'FollowSocial'), data).success(function(data) {
+          if (!data.LoggedIn){
+            window.location = 'http://' + window.location.hostname
+              + pathname
+              + 'SocLogin?service='
+              + service
+              + '&return_url='
+              + escape(window.location.hostname + window.location.pathname + window.location.search)
+              + '&follow_param=' + escape(param);
+          }
+          else if (data.error == 'no' && typeof (data.message) != 'undefined'){
+            angular.element('#' + idBlock + ' a.spot-button.soc-link').text(data.message);
+          }
+        });
+      }
+    });
+  </script>
 </body>
 </html>
