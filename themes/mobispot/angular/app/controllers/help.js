f4e9c206a964dfe88fa1fb24fd240fe5f0b7b9aa
Lyashkov Pavel <bilbo.kem@gmail.com>
2014-08-17 15:53:14 +0300

    fix

--- themes/mobispot/angular/app/controllers/help.js
+++ themes/mobispot/angular/app/controllers/help.js
@@ -9,6 +9,7 @@ angular.module('mobispot').controller('HelpController',
     if (!valid) return false;
     user.email = user.help_email;
     $http.post('/service/sendQuestion', user).success(function(data) {
+      if(data.error == 'no') {
         $scope.user.name = '';
         $scope.user.help_email = '';
         $scope.user.question = '';
@@ -16,6 +17,7 @@ angular.module('mobispot').controller('HelpController',
 
         $scope.result.message = data.content;
         contentService.viewModal('message');
+      }
     });
   };
 });
