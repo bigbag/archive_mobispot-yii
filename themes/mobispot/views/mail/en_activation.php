f953e45e355c0ab9d8d974862aee4fb01f511b97
Denis Amelin <dionicii@mail.ru>
2014-10-10 16:55:30 +0400

    feature: Новые шаблоны писем на активацию аккаунта

--- themes/mobispot/views/mail/en_activation.php
+++ themes/mobispot/views/mail/en_activation.php
@@ -1,19 +1,8 @@
-<div bgcolor="#ffffff" style="padding:30px 0;margin:0; width:660px;  font-family:Arial, sans-serif;font-size:12pt;">
-<div style="padding-left:30px">
-    <img src="<?php echo $this->getBaseUrl(); ?>/themes/mobispot/images/mail/mobispot.png"/>
-</div>
-<br /><br />
-<div style="background:#f1f3f4; padding:30px; width:600px;">
-    <h2 style="color: #0062ff;font-size:1.9em;">Регистрация на сайте <?php echo $this->getBaseUrl(); ?></h2>
-
-    <p>Для активации вашей учётной записи вам необходимо перейти по ссылке<br/>
-            <?php echo $activation_url; ?></p>
-</div>
-<table style="margin-top:40px; width:660px; padding:0 30px" >
-    <tr>
-        <td rowspan="2" style="text-align:left;align:left;">&copy; Mobispot. Ltd. All rights reserved<br>
-            sales@mobispot.com<br>
-        </td>
-    </tr>
-</table>
-</div>
+<p>Hello!</p>
+<p>You successfully registered your first spot on Mobispot</p>
+<p>To activate your account, please click on the link below<br/>
+<a href="<?php echo $activation_url; ?>" target="_blank"><?php echo $activation_url; ?></a></p>
+<p><br></p>
+<p>&copy;  Mobispot Social Systems Pte. Ltd.<br>
+<a href="mailto:hola@mobispot.com">hola@mobispot.com</a></p>
+<p>Follow us in <a href="https://www.facebook.com/heyMobispot" target="_blank">Facebook</a> or  <a href="https://twitter.com/heymobispot" target="_blank">Twitter</a></p>
