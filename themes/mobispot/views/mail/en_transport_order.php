2895e2aac3739fdb4c979d84d30c787ea7863a9b
Denis Amelin <dionicii@mail.ru>
2014-11-07 17:49:47 +0400

    feature: доработки кастомной транспортной карты, карта отсылается в .jpg

--- themes/mobispot/views/mail/en_transport_order.php
+++ themes/mobispot/views/mail/en_transport_order.php
@@ -7,50 +7,22 @@
         <img width="513" height="321" src="<?php echo $this->getBaseUrl(); ?>/uploads/transport/<?php echo $back_img; ?>">
     </div>
 </td>
-<td style="padding-left:10px;">
-    <table id="vertical" style="width:351px;
-    height: 513px;
-    border-radius: 30px;
-    background: none repeat scroll 0% 0% #E9EDF2;background-color: #FFF;border: 2px solid #E9EDF2;overflow: hidden;">
-    <tr>
-        <td valign="top" style="text-align:center; padding-top:30px; height:78px;">
-            <img src="<?php echo $this->getBaseUrl(); ?>/themes/mobispot/img/mail/card_horizontal_arm.png">
-        </td>
-    </tr>
-    <tr>
-        <td valign="middle" style="text-align:center; height:165px;overflow: hidden;">
-        <div style="border: 1px solid #E9EDF2;border-radius: 50%;display: inline-block;height: 165px;width: 165px;text-align: center;overflow: hidden;padding:0;margin:0;"> 
-            <img width="165" height="165" src="<?php echo $this->getBaseUrl(); ?><?php echo $photo; ?>">
-        </div>
-        </td>
-    </tr>
-    <tr>
-        <td valign="top" style="text-align:center; height:90px; padding-top:21px;font-family:museo_sans_cyrl100,Arial,Helvetica,sans-serif;font-size: 33px;color: rgba(0, 0, 0, 0.75);padding: 32px 35px 0px 35px;line-height: 40px;font-weight:500">
-            <?php echo $name; ?>
-        </td>
-    </tr>
-    <tr>
-        <td valign="top" style="text-align:center;height:42px;overflow: hidden;word-wrap: break-word;font-size: 18px;color: #8D9095;text-align: center;padding: 3px 60px 11px 60px;line-height:20px;font-weight:500">
-            <?php echo $position; ?>
-        </td>
-    </tr>
-    <tr>
-        <td valign="middle" style="text-align:center; height:60px">
-        <div style="display: inline-block;overflow:hidden;width:230px;height:60px;padding-bottom:-5px">
-                <img width="230" height="60" src="<?php echo $this->getBaseUrl(); ?><?php echo $logo; ?>">
-        </div>
-        </td>
-    </tr>
-    <tr>
-        <td>
-        </td>
-    </tr>
-    </table>
+</tr>
+<tr>
+<td style="vertical-align:top;text-align:left;">
+    <img width="321" height="513" src="<?php echo $this->getBaseUrl(); ?>/uploads/spot/<?php echo $front_img; ?>">
 </td>
 </tr>
 </table>
 <p>Данные доставки:</p>
 <table>
+<tr>
+<td>Email:
+</td>
+<td>
+<?php echo $email; ?>
+</td>
+</tr>
 <tr>
 <td>Имя:
 </td>
