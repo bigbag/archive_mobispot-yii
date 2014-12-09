d97a441cf0052866cc5353d4820db3c76c66a0e4
Denis Amelin <dionicii@mail.ru>
2014-12-09 13:52:19 +0400

    translate Итальянский язык

---
+++ protected/messages/it/help.php
@@ -0,0 +1,15 @@
+<?php return  array (
+  'Email' => 'E-mail',
+  'Get Help' => '',
+  'Have a question? Need a hand?
+                            Anything bugging you?
+                            Please stick your details in this form and we’ll get back to you. Pronto. If not sooner.' => '',
+  'Hook up at Facebook' => '',
+  'If you have a suggested answer, please let us know' => '',
+  'Keep up at Twitter' => '',
+  'Name' => 'Nome',
+  'Phone' => 'Telefono',
+  'Question' => '',
+  'Question has been submitted' => '',
+  'Send' => 'Mandare',
+);
---
+++ protected/messages/it/phone.php
@@ -0,0 +1,12 @@
+<?php return  array (
+  'Contactless readers' => 'Attenzione:',
+  'Device compatibility' => 'Apparecchi compatibili',
+  'Help' => '',
+  'Phones & tablets' => 'Telefoni e Tablet',
+  'Please note: ' => 'Attenzione: ',
+  'Readers' => '',
+  'Some devices specifications change regularly and depend on the sales region. For the most accurate information check with your retailer or mobile operator that NFC is enabled on your device.' => 'la specificazione di alcuni apparecchi può cambiare e dipende dalla regione di vendite. Per favore accerti al suo venditore oppure operatore di rete mobile se sopporta l’apparecchio la funzione di NFC.',
+  'Special app needed. These devices must have special app to read the spots:' => 'Bisogna un’applicazione speciale. Per lettura di spot su questi device dev’essere installata l’applicazione speciale:',
+  'Spots can be read by many popular phones and tablets with NFC functionality.<br> Here is the list of currently supported devices:' => 'Gli spot possono essere letti da maggioranza di apparecchi mobili popolari con la funzione NFC.<br> Ecco l’elenco degli apparecchi sopportabili al momento presente:',
+  'We have tested and verified compatibility with the following readers:' => 'Abbiamo controllato la compatibilità di Spot con tali reader:',
+);
