aca5114fe5647914d49142dfab1cee97f499c188
Lyashkov Pavel <bilbo.kem@gmail.com>
2014-12-10 12:42:46 +0300

    Added support for yandex metrica

--- themes/mobispot/views/layouts/block/script/all.php
+++ themes/mobispot/views/layouts/block/script/all.php
@@ -6,3 +6,5 @@
 <?php echo $this->blockFooterScript; ?>
 <script src="/themes/mobispot/js/script.min.js"></script>
 <script src="/themes/mobispot/js/foundation.min.js"></script>
+
+<noscript><div><img src="//mc.yandex.ru/watch/27425150" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
