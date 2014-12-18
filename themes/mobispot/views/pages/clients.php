2f75742c78f8a85b364344443f97d2fb6c18f4fe
Denis Amelin <dionicii@mail.ru>
2014-12-18 10:44:41 +0400

    fix

---
+++ themes/mobispot/views/pages/clients.php
@@ -0,0 +1,84 @@
+<?php $this->pageTitle = Yii::t('general', 'Наши клиенты'); ?>
+<?php $this->casingClass = 'clients'?>
+
+    <div class="content-wrapper">
+        <div class="content-block">
+        <div class="row">
+        <div class="large-12 columns form-block">
+                <div class="row demo-block">
+                    <div class="small-12 large-12 column">
+                        <h1><?php echo Yii::t('general', 'Наши клиенты');?></h1>
+                        <p class="form-h clearfix">
+                            <?php echo Yii::t('general', 'Компании, которые пользуются бесконтактными устройствами и приложениями "Мобиспот",<br>
+                            интегрируя множество приложений в один удобный носитель.');?>
+                        </p>
+                        <ul class="clients-list">
+                            <li id="east">
+                                <div class="wrapper">
+                                    <a href="http://www.eastbalt.ru/"><?php echo Yii::t('general', 'East Balt');?></a>
+                                    <span><?php echo Yii::t('general', 'В кампусную карту интегрированы:');?></span>
+                                    <ul>
+                                        <li><?php echo Yii::t('general', 'Контроль доступа');?></li>
+                                        <li><?php echo Yii::t('general', 'Учет рабочего времни');?></li>
+                                        <li><?php echo Yii::t('general', 'Корпоративное питание');?></li>
+                                        <li><?php echo Yii::t('general', 'Учет потребления кофе в вендинговых аппаратах');?></li>
+                                    </ul>
+                                </div>
+                            </li>
+                            <li id="quartz" style=" background-image: url(/themes/mobispot/img/logos/quartz_logo.png)">
+                                <div class="wrapper">
+                                    <a href="http://www.quartz-trans.com/"><?php echo Yii::t('general', 'Кварц-Транс');?></a>
+                                    <span><?php echo Yii::t('general', 'В кампусную карту интегрированы:');?></span>
+                                    <ul>
+                                        <li><?php echo Yii::t('general', 'Учет рабочего времни');?></li>
+                                        <li><?php echo Yii::t('general', 'Корпаративное питание');?></li>
+                                        <li><?php echo Yii::t('general', 'Учет потребления кофе в вендинговых аппаратах');?></li>
+                                    </ul>
+                                </div>
+                            </li>
+                            <li id="qr">
+                                <div class="wrapper">
+                                    <a href="https://payqr.ru/"><?php echo Yii::t('general', 'PayQR');?></a>
+                                    <span><?php echo Yii::t('general', 'В кампусную карту интегрированы:');?></span>
+                                    <ul>
+                                        <li><?php echo Yii::t('general', 'Контроль доступа');?></li>
+                                        <li><?php echo Yii::t('general', 'Учет рабочего времни');?></li>
+                                        <li><?php echo Yii::t('general', 'Учет потребления кофе в вендинговых аппаратах');?></li>
+                                    </ul>
+                                </div>
+                            </li>
+                            <li id="thr">
+                                <div class="wrapper">
+                                    <a href="http://www.3rmemory.ru/"><?php echo Yii::t('general', '3R');?></a>
+                                    <span><?php echo Yii::t('general', 'В кампусную карту интегрированы:');?></span>
+                                    <ul>
+                                        <li><?php echo Yii::t('general', 'Контроль доступа');?></li>
+                                        <li><?php echo Yii::t('general', 'Учет рабочего времни');?></li>
+                                        <li><?php echo Yii::t('general', 'Учет потребления кофе в вендинговых аппаратах');?></li>
+                                    </ul>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="row demo-block">
+                    <div class="small-12 large-12 column">
+                        <h1><?php echo Yii::t('general', 'Наши партнеры');?></h1>
+                        <p class="form-h clearfix">
+                            <?php echo Yii::t('general', 'Компании, которые при помощи наших сервисов создают добавленную ценность для своих <br>
+                            клиентов.');?>
+                        </p>
+                        <ul class="clients-list">
+                            <li id="barista">
+                                <div class="wrapper">
+                                    <a href="http://www.barista-ltd.ru/"><?php echo Yii::t('general', 'Бариста-Лтд');?></a>
+                                    <span><?php echo Yii::t('general', 'При помощи решений "Мобиспот" компания организовала прием платежей в собственной сети кофейных и снэковых аппаратов');?>.</span>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
