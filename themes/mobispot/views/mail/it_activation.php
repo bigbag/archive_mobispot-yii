162e76bcb520449be5f59a5ecefac9d114ad4df2
Denis Amelin <dionicii@mail.ru>
2015-01-23 10:25:03 +0300

    style: новые шаблоны почты для активации и восстановления пароля

--- themes/mobispot/views/mail/it_activation.php
+++ themes/mobispot/views/mail/it_activation.php
@@ -1,8 +1,8 @@
 <p>Ciao!</p>
-<p>È la registrazione della tua primo spot su Mobispot</p>
-<p>Per attivare il tuo account, clicca sul link qui sotto:<br/>
+<p style="line-height: 1.2">È la registrazione della tua primo spot su Mobispot</p>
+<p style="line-height: 1.2">Per attivare il tuo account, clicca sul link qui sotto:<br/>
 <a href="<?php echo $activation_url; ?>" target="_blank"><?php echo $activation_url; ?></a></p>
 <p><br></p>
-<p>&copy;  Mobispot Social Systems Pte. Ltd.<br>
+<p style="line-height: 1.2">&copy;  Mobispot Social Systems Pte. Ltd.<br>
 <a href="mailto:hola@mobispot.com">hola@mobispot.com</a></p>
-<p>Follow us in <a href="https://www.facebook.com/heyMobispot" target="_blank">Facebook</a> or  <a href="https://twitter.com/heymobispot" target="_blank">Twitter</a></p>
+<p style="line-height: 1.2">Follow us in <a href="https://www.facebook.com/heyMobispot" target="_blank">Facebook</a> or  <a href="https://twitter.com/heymobispot" target="_blank">Twitter</a></p>
